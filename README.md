44108feaf65535e164f50f1c3133a522ec9bb84b
mithridatem <mathieumithridate@adrar-formation.com>
2026-03-17 11:01:03 +0100

    feat: add env.php

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # php-pdo-dev312501
+## 1 Installer les dépendances 
+```sh
+composer install
+```
+
+## 2 créer le fichier env.php
+```php
+<?php
+
+const DB_HOST = "localhost";
+const DB_USERNAME = "";
+const DB_PASSWORD = "";
+const DB_NAME = "";
+```
--- database.php
+++ database.php
@@ -2,10 +2,13 @@
 
 function connect(): PDO
 {
-     return new PDO(
-        'mysql:host=localhost;dbname=demo', 
-        'root',
-        'root', 
+    //Import du fichier environnement
+    include 'env.php';
+    //Création d'un objet PDO
+    return new PDO(
+        'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . '',
+        DB_USERNAME,
+        DB_PASSWORD,
         [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
     );
 }
